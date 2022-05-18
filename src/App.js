1986f086be14904ee90d4ef76701538af1353e69
abdul-rehman67 <ar6373150@gmail.com>
2022-05-18 00:00:28 -0700

    minor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import React from 'react';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
    <div>
-     hello
+     <Navbar/>
+     
 
    </div>
   );
